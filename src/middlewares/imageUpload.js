9ce1f6b7842f300208ab784fe462d81e1c35467a
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-27 09:29:30 -0300

    feat: este commit adiciona o middleware de upload de imagens.

---
+++ src/middlewares/imageUpload.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+const path = require("path");
+
+const imageStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    let folder = "";
+
+    if (req.baseUrl.includes("users")) {
+      folder = "users";
+    } else if (req.baseUrl.includes("photos")) {
+      folder = "photos";
+    }
+
+    callback(null, `uploads/${folder}`);
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(res, file, callback) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      return callback(new Error("As imagens devem ser no formato png ou jpg."));
+    }
+    callback(undefined, true);
+  },
+});
+
+module.exports = {
+  imageUpload,
+};
