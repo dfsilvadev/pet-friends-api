413fe6a8cb198d9c469d5c3c476b4451fbdb1812
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-29 19:45:37 -0300

    feat: este commit adiciona a funcionalidade de pesquisa de foto.

--- src/routes/Photo.js
+++ src/routes/Photo.js
@@ -10,6 +10,7 @@ const {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 } = require("../controllers/Photo");
 
 const validate = require("../middlewares/handleValidation");
@@ -33,6 +34,7 @@ router.post(
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/search", authGuard, searchPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
