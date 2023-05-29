f719fe64f1cba9f4c83e9832167aa9640e5a9917
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-29 18:28:23 -0300

    feat: este commit adiciona o método que busca todas as fotos.

--- src/routes/Photo.js
+++ src/routes/Photo.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { insertPhoto, deletePhoto } = require("../controllers/Photo");
+const {
+  insertPhoto,
+  deletePhoto,
+  getAllPhotos,
+} = require("../controllers/Photo");
 
 const validate = require("../middlewares/handleValidation");
 const authGuard = require("../middlewares/authGuard");
@@ -18,5 +22,6 @@ router.post(
   insertPhoto
 );
 router.delete("/:id", authGuard, deletePhoto);
+router.get("/", getAllPhotos);
 
 module.exports = router;
